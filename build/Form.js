639bbf4465b8dd785ad14901b5cc86b80a25411a
liushzh <liushzh@yonyou.com>
2017-10-28 18:35:53 +0800

    publish 2.0.1 and

--- build/Form.js
+++ build/Form.js
@@ -1,14 +1,8 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-  value: true
+    value: true
 });
-
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
-var _classnames = require('classnames');
-
-var _classnames2 = _interopRequireDefault(_classnames);
 
 var _react = require('react');
 
@@ -18,11 +12,25 @@ var _propTypes = require('prop-types');
 
 var _propTypes2 = _interopRequireDefault(_propTypes);
 
+var _beeButton = require('bee-button');
+
+var _beeButton2 = _interopRequireDefault(_beeButton);
+
+var _beeFormGroup = require('bee-form-group');
+
+var _beeFormGroup2 = _interopRequireDefault(_beeFormGroup);
+
+var _beeLabel = require('bee-label');
+
+var _beeLabel2 = _interopRequireDefault(_beeLabel);
+
+var _classnames = require('classnames');
+
+var _classnames2 = _interopRequireDefault(_classnames);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 function _defaults(obj, defaults) { var keys = Object.getOwnPropertyNames(defaults); for (var i = 0; i < keys.length; i++) { var key = keys[i]; var value = Object.getOwnPropertyDescriptor(defaults, key); if (value && value.configurable && obj[key] === undefined) { Object.defineProperty(obj, key, value); } } return obj; }
-
-function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
@@ -31,56 +39,171 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : _defaults(subClass, superClass); }
 
 var propTypes = {
-  horizontal: _propTypes2["default"].bool,
-  inline: _propTypes2["default"].bool,
-  componentClass: _propTypes2["default"].oneOfType([_propTypes2["default"].element, _propTypes2["default"].string])
+    clsPrefix: _propTypes2["default"].string,
+    className: _propTypes2["default"].string,
+    submitError: _propTypes2["default"].func, //form验证失败的回调
+    submitSuccess: _propTypes2["default"].func, //form验证成功的回调
+    submitAreaClassName: _propTypes2["default"].string, //提交区域className
+    submitBtnClassName: _propTypes2["default"].string, //提交按钮className
+    beforeSubmitBtn: _propTypes2["default"].element, //提交按钮之前的dom
+    afterSubmitBtn: _propTypes2["default"].element //提交按钮之后的dom
 };
-
 var defaultProps = {
-  horizontal: false,
-  inline: false,
-  componentClass: 'form',
-  clsPrefix: 'u-form'
+    clsPrefix: 'u-form',
+    className: '',
+    submitError: function submitError() {}, //form验证失败的回调
+    submitSuccess: function submitSuccess() {}, //form验证成功的回调
+    submitAreaClassName: '',
+    submitBtnClassName: '',
+    beforeSubmitBtn: '',
+    afterSubmitBtn: ''
 };
 
-var Form = function (_React$Component) {
-  _inherits(Form, _React$Component);
+var Form = function (_Component) {
+    _inherits(Form, _Component);
 
-  function Form() {
-    _classCallCheck(this, Form);
+    function Form(props) {
+        _classCallCheck(this, Form);
 
-    return _possibleConstructorReturn(this, _React$Component.apply(this, arguments));
-  }
+        var _this = _possibleConstructorReturn(this, _Component.call(this, props));
 
-  Form.prototype.render = function render() {
-    var _props = this.props,
-        horizontal = _props.horizontal,
-        inline = _props.inline,
-        Component = _props.componentClass,
-        className = _props.className,
-        clsPrefix = _props.clsPrefix,
-        others = _objectWithoutProperties(_props, ['horizontal', 'inline', 'componentClass', 'className', 'clsPrefix']);
+        _this.checkItem = function (obj, flag) {
+            var items = _this.state.items;
+            items.forEach(function (item) {
+                if (item.name === obj.name) {
+                    item.verify = obj.verify;
+                    item.value = obj.value || '';
+                }
+            });
+            _this.setState({
+                items: items
+            });
+            if (flag && items[items.length - 1] && items[items.length - 1].name === obj.name) {
+                _this.submit(items);
+            }
+        };
 
-    var classes = {};
+        _this.getFormItems = function () {
+            var items = [];
+            if (_this.props.children.length) {
+                _this.props.children.map(function (item) {
+                    if (item.props.isFormItem) {
+                        items.push({
+                            'name': item.props.children.props.name,
+                            'verify': true,
+                            'value': ''
+                        });
+                    }
+                });
+            } else {
+                var item = _this.props.children;
+                if (item.props.isFormItem) {
+                    items.push({
+                        'name': item.props.children.props.name,
+                        'verify': true,
+                        'value': ''
+                    });
+                }
+            }
 
-    if (inline) {
-      classes[clsPrefix + '-inline'] = true;
+            _this.setState({
+                items: items
+            });
+        };
+
+        _this.checkNow = function () {
+            _this.setState({
+                checkNow: true
+            });
+        };
+
+        _this.submit = function (items) {
+            var flag = true;
+            items.forEach(function (item) {
+                if (!item.verify) {
+                    flag = false;
+                }
+            });
+            if (flag) {
+                _this.setState({
+                    checkNow: false
+                });
+                _this.props.submitSuccess(_this.state.items);
+            } else {
+                _this.setState({
+                    checkNow: false
+                });
+                _this.props.submitError(_this.state.items);
+            }
+        };
+
+        _this.state = {
+            items: [], //验证结果对象
+            checkNow: false //是否立刻验证，提交按钮
+        };
+        return _this;
     }
-    if (horizontal) {
-      classes[clsPrefix + '-horizontal'] = true;
-    }
 
-    var classnames = (0, _classnames2["default"])(classes, clsPrefix);
-    return _react2["default"].createElement(Component, _extends({}, others, {
-      className: (0, _classnames2["default"])(className, classnames)
-    }));
-  };
+    Form.prototype.componentDidMount = function componentDidMount() {
+        this.getFormItems();
+    };
 
-  return Form;
-}(_react2["default"].Component);
+    Form.prototype.render = function render() {
+        var _this2 = this;
 
+        var childs = [];
+        _react2["default"].Children.map(this.props.children, function (child) {
+            if (child.props.isFormItem) {
+                childs.push(_react2["default"].createElement(
+                    _beeFormGroup2["default"],
+                    null,
+                    _react2["default"].createElement(
+                        _beeLabel2["default"],
+                        null,
+                        child.props.labelName
+                    ),
+                    child.props.inputAfore,
+                    _react2["default"].cloneElement(child, {
+                        checkItem: _this2.checkItem,
+                        checkNow: _this2.state.checkNow
+                    }),
+                    child.props.inputBefore
+                ));
+            } else {
+                childs.push(_react2["default"].cloneElement(child));
+            }
+        });
+        var _props = this.props,
+            className = _props.className,
+            submitAreaClassName = _props.submitAreaClassName,
+            submitBtnClassName = _props.submitBtnClassName,
+            beforeSubmitBtn = _props.beforeSubmitBtn,
+            afterSubmitBtn = _props.afterSubmitBtn,
+            clsPrefix = _props.clsPrefix;
+
+        return _react2["default"].createElement(
+            'form',
+            { className: clsPrefix + ' ' + className, onSubmit: this.checkNow },
+            childs,
+            _react2["default"].createElement(
+                'div',
+                { className: clsPrefix + '-submit ' + submitAreaClassName },
+                beforeSubmitBtn,
+                _react2["default"].createElement(
+                    _beeButton2["default"],
+                    { onClick: this.checkNow, colors: 'primary', className: clsPrefix + '-submit-btn ' + submitBtnClassName },
+                    '\u63D0\u4EA4'
+                ),
+                afterSubmitBtn
+            )
+        );
+    };
+
+    return Form;
+}(_react.Component);
+
+;
 Form.propTypes = propTypes;
 Form.defaultProps = defaultProps;
-
 exports["default"] = Form;
 module.exports = exports['default'];
---
+++ build/FormItem.js
@@ -0,0 +1,228 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reactDom = require('react-dom');
+
+var _reactDom2 = _interopRequireDefault(_reactDom);
+
+var _propTypes = require('prop-types');
+
+var _propTypes2 = _interopRequireDefault(_propTypes);
+
+var _classnames = require('classnames');
+
+var _classnames2 = _interopRequireDefault(_classnames);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+function _defaults(obj, defaults) { var keys = Object.getOwnPropertyNames(defaults); for (var i = 0; i < keys.length; i++) { var key = keys[i]; var value = Object.getOwnPropertyDescriptor(defaults, key); if (value && value.configurable && obj[key] === undefined) { Object.defineProperty(obj, key, value); } } return obj; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : _defaults(subClass, superClass); }
+
+var regs = {
+    email: /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/,
+    tel: /^1[3|4|5|7|8]\d{9}$/,
+    IDCard: /^(\d{15}$|^\d{18}$|^\d{17}(\d|X|x))$/, //身份证
+    name: /^[\u4e00-\u9fa5]+?$/,
+    password: /^[0-9a-zA-Z,.!?`~#$%^&*()-=_+<>'"\[\]\{\}\\\|]{6,15}$/ //6-15位数字
+};
+var propTypes = {
+    clsPrefix: _propTypes2["default"].string,
+    className: _propTypes2["default"].string,
+    isRequire: _propTypes2["default"].bool, //是否必填
+    errorMessage: _propTypes2["default"].string, //错误信息
+    htmlType: _propTypes2["default"].oneOf(['email', 'tel', 'IDCard', 'name', 'password', null]), //htmlType有值的时候 reg不生效
+    reg: _propTypes2["default"].instanceOf(RegExp), //校验正则
+    method: _propTypes2["default"].oneOf(['change', 'blur', null]), //校验方式
+    blur: _propTypes2["default"].func, //失去焦点的回调,参数为value
+    change: _propTypes2["default"].func, //值改变的回调,参数为value
+    checkSuccess: _propTypes2["default"].func, //验证成功的回调
+    checkError: _propTypes2["default"].func, //验证失败的回调
+    checkItem: _propTypes2["default"].func,
+    inline: _propTypes2["default"].bool, //formItem是否行内
+    labelName: _propTypes2["default"].string //label标签文字
+};
+var defaultProps = {
+    clsPrefix: 'u-form',
+    isRequire: false, //是否必填
+    errorMessage: '校验失败', //错误信息
+    reg: /[/w/W]*/,
+    method: 'change',
+    blur: function blur() {},
+    change: function change() {},
+    isFormItem: true,
+    checkSuccess: function checkSuccess() {},
+    checkError: function checkError() {},
+    checkItem: function checkItem() {},
+    inline: false,
+    labelName: ''
+};
+
+var FormItem = function (_Component) {
+    _inherits(FormItem, _Component);
+
+    function FormItem(props) {
+        _classCallCheck(this, FormItem);
+
+        var _this = _possibleConstructorReturn(this, _Component.call(this, props));
+
+        _this.handleBlur = function () {
+            var value = _reactDom2["default"].findDOMNode(_this.refs.input).value;
+            var name = _reactDom2["default"].findDOMNode(_this.refs.input).name;
+            var flag = _this.itemCheck(value, name);
+            if (_this.props.method === 'blur') {
+                _this.setState({
+                    hasError: !flag
+                });
+                _this.props.checkItem({
+                    "name": name,
+                    "verify": flag,
+                    "value": value
+                });
+            }
+            _this.props.blur(value);
+        };
+
+        _this.handleChange = function (selectV) {
+            var value = selectV || _reactDom2["default"].findDOMNode(_this.refs.input).value || _this.refs.input.props.defaultValue;
+            var name = _reactDom2["default"].findDOMNode(_this.refs.input).name || _this.refs.input.props.name;
+            _this.setState({
+                value: value
+            });
+            var flag = _this.itemCheck(value, name);
+            if (_this.props.method === 'change') {
+                _this.setState({
+                    hasError: !flag
+                });
+                _this.props.checkItem({
+                    "name": name,
+                    "verify": flag,
+                    "value": value
+                });
+            }
+            _this.props.change(value);
+        };
+
+        _this.itemCheck = function (value, name) {
+            var _this$props = _this.props,
+                isRequire = _this$props.isRequire,
+                htmlType = _this$props.htmlType;
+
+            var reg = htmlType ? regs[htmlType] : _this.props.reg;
+            var flag = reg.test(value);
+            var obj = {
+                "name": name,
+                "verify": flag,
+                "value": value
+            };
+            if (isRequire) {
+                if (value) {
+                    flag ? _this.props.checkSuccess(obj) : _this.props.checkError(obj);
+                    return flag;
+                } else {
+                    _this.props.checkError(obj);
+                    return false;
+                }
+            } else {
+                _this.props.checkSuccess(obj);
+                return true;
+            }
+        };
+
+        _this.checkSelf = function () {
+            var value = _reactDom2["default"].findDOMNode(_this.refs.input).value || _this.state.value || _this.refs.input.domValue || _this.refs.input.props.defaultValue;
+            var name = _reactDom2["default"].findDOMNode(_this.refs.input).name || _this.refs.input.props.name;
+            var flag = _this.itemCheck(value, name);
+            _this.props.checkItem({
+                "name": name,
+                "verify": flag,
+                "value": value
+            }, true);
+            _this.setState({
+                hasError: !flag
+            });
+        };
+
+        _this.state = {
+            hasError: false,
+            value: ''
+        };
+        return _this;
+    }
+
+    FormItem.prototype.componentDidMount = function componentDidMount() {
+        console.log(this);
+    };
+
+    FormItem.prototype.componentWillReceiveProps = function componentWillReceiveProps(nextProps) {
+        if (nextProps.checkNow && !this.props.checkNow) {
+            this.checkSelf();
+        }
+    };
+
+    /**
+     * 校验方法
+     * @param value
+     * @returns {boolean}
+     */
+
+    /**
+     * 触发校验
+     */
+
+
+    FormItem.prototype.render = function render() {
+        var _props = this.props,
+            children = _props.children,
+            inline = _props.inline,
+            errorMessage = _props.errorMessage,
+            className = _props.className,
+            clsPrefix = _props.clsPrefix;
+
+        var clsObj = {};
+        clsObj[clsPrefix + '-item'] = true;
+        clsObj[className] = true;
+        var clsErrObj = {};
+        clsErrObj[clsPrefix + '-error'] = true;
+        if (inline) {
+            clsObj[clsPrefix + '-inline'] = true;
+            clsErrObj[clsPrefix + '-error-inline'] = true;
+        }
+        if (this.state.hasError) clsErrObj['show'] = true;
+
+        return _react2["default"].createElement(
+            'div',
+            { className: (0, _classnames2["default"])(clsObj) },
+            _react2["default"].cloneElement(children, {
+                onBlur: this.handleBlur,
+                onChange: this.handleChange,
+                ref: 'input',
+                domValue: children.props.defaultValue || ''
+            }),
+            _react2["default"].createElement(
+                'div',
+                { className: (0, _classnames2["default"])(clsErrObj) },
+                errorMessage
+            )
+        );
+    };
+
+    return FormItem;
+}(_react.Component);
+
+;
+FormItem.propTypes = propTypes;
+FormItem.defaultProps = defaultProps;
+exports["default"] = FormItem;
+module.exports = exports['default'];
